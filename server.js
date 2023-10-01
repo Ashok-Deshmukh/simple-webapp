a24871938383d81656856dafbd6aade3a07cd22b
Ashok Deshmukh <ashokdeshmukh@Ashoks-MacBook-Air.local>
2023-10-01 03:45:17 -0400

    const express = require('express'); duplicated like a retard lol

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const path = require('path'); 
-const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
